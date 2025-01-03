3fae84384fce3a414a7857071f8ab0a10ec8bee2
Evgeniy Dmitriev <dmitriew85@yandex.ru>
2025-01-03 13:33:27 +0300

    feat[HistoryManager]: add Node class, head & tail fields, linked HashMap, methods

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -2,7 +2,11 @@ import java.util.List;
 
 public interface HistoryManager {
     // Добавление задачи в список истории
-   void add(Task task);
+    void add(Task task);
+
+    // Удаление задачи из списка истории
+    void remove(int id);
+
     // Получение списка истории
-   List<Task> getHistory();
+    List<Task> getHistory();
 }
--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int MAX_HISTORY_SIZE = 10;
-    private final List<Task> history;
+    private final Map<Integer, Node> history;
+    private Node head;
+    private Node tail;
 
     public InMemoryHistoryManager() {
-        this.history = new ArrayList<>();
+        this.history = new HashMap<>();
     }
 
     // Добавление задачи в список истории
@@ -15,15 +18,84 @@ public class InMemoryHistoryManager implements HistoryManager {
         if (task == null) {
             return;
         }
-        while (history.size() >= MAX_HISTORY_SIZE) {
-            history.removeFirst();
+        remove(task.getId());
+        linkLast(task);
+        history.put(task.getId(), tail);
+    }
+
+    // Удаление задачи из списка истории
+    @Override
+    public void remove(int id) {
+        Node node = history.get(id);
+        if (node == null) {
+            return;
         }
-        history.add(task);
+        removeNode(node);
+        history.remove(id);
     }
 
     // Получение списка истории
     @Override
     public List<Task> getHistory() {
-        return history;
+        return getTasks();
+    }
+
+    // Добавление задачи в конец списка
+    private void linkLast(Task task) {
+        if (task == null) {
+            return;
+        }
+        if (tail == null) {
+            head = new Node(null, task, null);
+            tail = head;
+        } else {
+            tail.next = new Node(tail, task, null);
+            tail = tail.next;
+        }
+    }
+
+    // Получение списка задач истории
+    private List<Task> getTasks() {
+        List<Task> historyList = new ArrayList<>();
+        Node currentNode = head;
+        while (currentNode != null) {
+            historyList.add(currentNode.task);
+            currentNode = currentNode.next;
+        }
+        return historyList;
+    }
+
+    // Удаление ноды
+    private void removeNode(Node node) {
+        if (node == null) {
+            return;
+        }
+        if (node == head && node == tail) {
+            head = null;
+            tail = null;
+        }
+        if (node.prev == null) {
+            head = node.next;
+        } else {
+            node.prev.next = node.next;
+        }
+        if (node.next == null) {
+            tail = node.prev;
+        } else {
+            node.next.prev = node.prev;
+        }
+    }
+
+    // Класс узлов связного списка
+    private static class Node {
+        Task task;
+        Node prev;
+        Node next;
+
+        Node(Node prev, Task task, Node next) {
+            this.task = task;
+            this.prev = prev;
+            this.next = next;
+        }
     }
 }
